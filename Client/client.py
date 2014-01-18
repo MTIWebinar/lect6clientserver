e231a9ae3419d95978638c8e7f3dd2996d2ff2ff
Kirill Osipov <osipovka@gmail.com>
2014-01-18 11:57:42 +0400

    [bf] python 3 support

--- Client/client.py
+++ Client/client.py
@@ -1,3 +1,5 @@
+# -*- coding:utf-8 -*-
+
 """
 Задание:
 1. один создал сервака с некой логикой
@@ -10,21 +12,22 @@ import socket
 
 import sys
 
-HOST, PORT = '78.47.201.69', 4001
-# локальная машина 127.0.0.1
+# HOST, PORT = '78.47.201.69', 4001
+HOST, PORT = '127.0.0.1', 4001
+
 data = ' '.join(sys.argv[1:]) # первый агрумент - это путь
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
     sock.connect((HOST, PORT))
-    sock.sendall(data + '\n')
+    sock.sendall((data + '\n').encode('utf-8'))
 
     received = sock.recv(1024)
 finally:
     sock.close()
 
 print('Sent: {}'.format(data))
-print('Received: {}'.format(received))
+print('Received: {}'.format(received.decode('utf-8')))
 
 # писать в файл то, что пришло с сервака
--- Server/server.py
+++ Server/server.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python
+# -*- coding:utf-8 -*-
 
-import SocketServer
+try:
+    SocketServer as socketserver
+except ImportError:
+    import socketserver
 
 class MyTCPHandler(SocketServer.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print("{} wrote:".format(self.client_address[0]))
+        decoded = self.data.decode('utf-8')
         print(self.data)
-        self.request.sendall(self.data.upper())
+        self.request.sendall(decoded.upper().encode('utf-8'))
         # self.request.sendall(len(self.data))
 
 
 if __name__ == '__main__':
     HOST, PORT = '0.0.0.0', 4001
-    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
 
     server.serve_forever()
